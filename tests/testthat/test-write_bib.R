215248fb8dbcfeec5e3a3ce73cb69fab2d28b4aa
Miguel Alvarez <kamapu78@gmail.com>
2021-12-17 11:30:02 +0000

    version 0.0.6

--- tests/testthat/test-write_bib.R
+++
@@ -1,9 +0,0 @@
-context("writing backups")
-
-test_that("Backups are written", {
-			Bib <- read_bib(x = file.path(path.package("biblio"),
-							"LuebertPliscoff.bib"))
-      File <- tempfile(fileext = ".bib")
-			write_bib(x = Bib, file = File)
-			expect_true(file.exists(File))
-		})
