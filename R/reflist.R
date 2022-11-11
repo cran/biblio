d286d303a612a95eb2eb28ecdc50490ace9e7c83
Miguel Alvarez <kamapu78@gmail.com>
2022-11-11 18:20:06 +0000

    version 0.0.7

--- R/reflist.R
+++ R/reflist.R
@@ -1,108 +1,113 @@
 #' @name reflist
-#' 
 #' @rdname reflist
-#' 
+#'
 #' @title Write a Reference List in rmarkdown
-#' 
-#' @description 
+#'
+#' @description
 #' A fast way to produce a reference list in an r-markdown document from a
 #' `lib_df` object.
-#' 
+#'
 #' This function may or may not produce intermediate files (bib and Rmd) and the
 #' result can be assigned to an object for further edition
 #' (see [yamlme::update()]).
-#' 
+#'
 #' A html file will be written by [write_rmd()] and [render_rmd()] in the
 #' working directory and displayed by [browseURL()].
-#' 
+#'
 #' @param x A `lib_df` object to produce the reference list. In the character
 #'     method, a character value indicating the path of a bibtex file
 #'     (passed to [biblio::read_bib()]).
 #' @param filename A character value with the name for the written Rmd file,
 #'     without file extension.
-#' @param bib_file A character value with the name for the written bibtex file.
-#'     In the lib_df method it can be omitted and will then named by
-#'     [tempfile()]. In the character method it is not required.
-#' @param delete_rmd A logical value indicating whether written Rmd file should
-#'     be deleted after rendering html or not.
-#' @param delete_bib A logical value indicating whether written bib file should
+#' @param output_file A character value with the name for the written Rmarkdown
+#'     file.
+#' @param delete_source A logical value indicating whether written bib file should
 #'     be deleted after rendering html or not.
 #' @param encoding A character value indicating the encoding string. It is
-#'     passed to [biblio::write_bib()].
+#'     passed to [write_bib()].
 #' @param title,output,nocite,urlcolor Arguments used for the yaml-header in
 #'     r-markdown and passed to [write_rmd()]. They can be cancelled using the
 #'     value NULL (not recommended for nocite).
-#' @param ... Further arguments passed to [write_rmd()] by the lib_df method, or
-#'     to the lib_df method by the character method.
-#' 
-#' @return 
+#' @param ... Further arguments passed to the yaml header in the intermediate
+#'     Rmarkdown document.
+#'
+#' @return
 #' An invisible object of class `rmd_doc`. A Rmd file will be written by
 #' [write_rmd()] as well.
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' reflist(synopsis)
 #' }
-#' 
-#' @exportMethod reflist
-setGeneric("reflist",
-		function(x, ...)
-			standardGeneric("reflist")
-)
+#'
+#' @export
+reflist <- function(x, ...) {
+  UseMethod("reflist", x)
+}
 
 #' @rdname reflist
-#' 
-#' @aliases reflist,lib_df-method
-#' 
-setMethod("reflist", signature(x = "lib_df"),
-		function(x, filename = "references", bib_file, delete_rmd = FALSE,
-				delete_bib = delete_rmd, title = "Automatic Reference List",
-        output = "html_document", nocite = "'@*'", urlcolor = "blue",
-        encoding = "UTF-8", ...) {
-			# write bib file
-			if(missing(bib_file))
-				bib_file <- tempfile(pattern = "ref", tmpdir = ".",
-						fileext = ".bib")
-			N <- nchar(bib_file)
-			if(substring(tolower(bib_file), N - 3, N) != ".bib")
-				bib_file <- paste0(bib_file, ".bib")
-			write_bib(x = x, file = bib_file, encoding = encoding)
-			# write Rmd file
-			filename <- paste0(filename, ".Rmd")
-			rmd_document <- write_rmd(title = title, output = output,
-					bibliography = bib_file, nocite = nocite,
-					urlcolor = urlcolor, filename = filename, ...)
-			# delete intermediary files
-			w_files <- c(filename, bib_file)
-			if(delete_rmd) {
-				file.remove(filename)
-				w_files <- w_files[w_files != filename]
-			}
-			if(delete_bib) {
-				file.remove(bib_file)
-				w_files <- w_files[w_files != bib_file]
-			}
-			# message for intermediary files
-			if(length(w_files) > 0)
-				message(paste0("\n## Intermediary files:\n   ", paste0(w_files,
-										collapse = "\n   "), "\n"))
-			# return rmd_doc xect if necessary
-			invisible(rmd_document)
-		})
-
-#' @rdname reflist
-#' 
 #' @aliases reflist,character-method
-#' 
-setMethod("reflist", signature(x = "character"),
-		function(x, filename = "references", ...) {
-			# Exchange arguments
-			bib_file <- x
-			N <- nchar(bib_file)
-			if(substring(tolower(bib_file), N - 3, N) != ".bib")
-				bib_file <- paste0(bib_file, ".bib")
-			x <- read_bib(bib_file)
-			# Execute lib_df method
-			reflist(x = x, filename = filename, bib_file = bib_file,
-					delete_bib = FALSE, ...)
-		})
+#' @method reflist character
+#' @export
+reflist.character <- function(x, output_file, delete_source = TRUE,
+                              title = "Automatic Reference List",
+                              output = "html_document",
+                              nocite = "'@*'", urlcolor = "blue",
+                              encoding = "UTF-8", ...) {
+  # Bibliography to tempdir
+  x <- paste0(file_path_sans_ext(x), ".bib")
+  if (missing(output_file)) {
+    output_file <- file_path_sans_ext(x)
+  }
+  file.copy(from = x, to = tempdir())
+  # Write Rmd file
+  bib_rmd <- as(list(
+    title = title,
+    output = output,
+    bibliography = basename(x),
+    nocite = nocite,
+    urlcolor = urlcolor,
+    encoding = encoding,
+    ...
+  ), "rmd_doc")
+  # Render Rmd file
+  render_rmd(bib_rmd, output_file = file.path(tempdir(), basename(output_file)))
+  # Copy Rmarkdown document and bibliography
+  if (!delete_source) {
+    file.copy(
+      from = file.path(tempdir(), paste0(basename(output_file), ".Rmd")),
+      to = dirname(output_file)
+    )
+    if (dirname(x) != dirname(output_file)) {
+      file.copy(from = x, to = dirname(output_file))
+    }
+  }
+  # return rmd_doc if necessary
+  invisible(bib_rmd)
+}
+
+#' @rdname reflist
+#' @aliases reflist,lib_df-method
+#' @method reflist lib_df
+#' @export
+reflist.lib_df <- function(x, filename, delete_source = TRUE, ...) {
+  # set filename
+  if (missing(filename)) {
+    filename <- paste0(deparse(substitute(x)))
+  } else {
+    filename <- file_path_sans_ext(filename)
+  }
+  write_bib(x, filename = file.path(tempdir(), basename(filename)))
+  # Render the reference list
+  obj <- reflist(file.path(tempdir(), basename(filename)), ...)
+  Files <- list.files(tempdir())
+  Files <- Files[grepl(basename(filename), Files, fixed = TRUE)]
+  if (delete_source) {
+    Files <- Files[!Files %in% paste0(basename(filename), c(".bib", ".Rmd"))]
+  }
+  file.copy(
+    from = file.path(tempdir(), Files),
+    to = dirname(filename)
+  )
+  invisible(obj)
+}
--- tests/testthat/test-read_bib.R
+++
@@ -1,6 +0,0 @@
-context("reading bibtex files")
-
-test_that("bibtex files are read", {
-			expect_is(read_bib(x = file.path(path.package("biblio"),
-									"LuebertPliscoff.bib")), "lib_df")
-		})
